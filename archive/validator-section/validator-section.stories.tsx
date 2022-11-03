dd796ae73ed3755b8a33a3e3d2da5bb9700d244d
darken141 <darken141@gmail.com>
2022-11-03 20:21:36 +0100

    update

--- archive/validator-section/validator-section.stories.tsx
+++ archive/validator-section/validator-section.stories.tsx
@@ -1,27 +1,27 @@
-// Utils
-import { Story, Meta } from "@storybook/react"
+// // Utils
+// import { Story, Meta } from "@storybook/react"
 
-// Components
-import ValidatorSection, { IProps } from "./validator-section.component"
+// // Components
+// import ValidatorSection, { IProps } from "./validator-section.component"
 
-// Data
-import kusamaData from "../../validators/kusama.json"
+// // Data
+// import kusamaData from "../../validators/kusama.json"
 
-// Types
-import { IValidator } from "../../types/validator.types"
+// // Types
+// import { IValidator } from "../../types/validator.types"
 
-const meta: Meta = {
-	title: "Sections/ValidatorSection",
-	component: ValidatorSection,
-}
+// const meta: Meta = {
+// 	title: "Sections/ValidatorSection",
+// 	component: ValidatorSection,
+// }
 
-export default meta
+// export default meta
 
-const Template: Story<IProps> = (args) => <ValidatorSection {...args} />
+// const Template: Story<IProps> = (args) => <ValidatorSection {...args} />
 
-export const Default = Template.bind({})
+// export const Default = Template.bind({})
 
-Default.args = {
-	data: kusamaData as unknown as IValidator,
-	enableAnimation: true,
-}
+// Default.args = {
+// 	data: kusamaData as unknown as IValidator,
+// 	enableAnimation: true,
+// }
