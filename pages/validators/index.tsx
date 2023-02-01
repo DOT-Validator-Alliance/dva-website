7f10496f1989a97656b450a690287b2fcb7e1f38
Darken141 <47373497+Darken141@users.noreply.github.com>
2023-02-01 01:29:51 -0800

    Update links with scroll property, Adjust robots.txt, adjust meta head (#2)
    * update links
    
    * Dev (#6)
    
    * Robots (#3)
    
    * update robots.txt
    
    * set up header, update robots config
    
    * Meta (#4)
    
    * create head component
    
    * update meta
    
    * Meta (#5)
    
    * create head component
    
    * update meta
    
    * update meta image

--- pages/validators/index.tsx
+++ pages/validators/index.tsx
@@ -8,11 +8,12 @@ import PageLayout from "../../layouts/page/page.layout"
 import ValidatorSection from "../../components/validator-section/validator-section.component"
 
 // Components
-import Head from "next/head"
+import Head from "../../components/head/head.component"
 
 // Hooks
 import { useTheme } from "styled-components"
 import { useMediaQuery } from "usehooks-ts"
+import { useRouter } from "next/router"
 
 // Types
 import { ReactElement } from "react"
@@ -20,58 +21,61 @@ import { IValidator } from "../../types/validator.types"
 import { GetStaticProps } from "next"
 
 export const getStaticProps: GetStaticProps = async () => {
-	const requireAll = (r: any) => r.keys().map(r)
-	const validators: IValidator[] = requireAll(
-		require.context("../../validators", false, /\.json$/)
-	)
+  const requireAll = (r: any) => r.keys().map(r)
+  const validators: IValidator[] = requireAll(
+    require.context("../../validators", false, /\.json$/)
+  )
 
-	const uniqueValidators = validators.filter(
-		(validator, index, self) =>
-			index === self.findIndex((t) => t.id === validator.id)
-	)
+  const uniqueValidators = validators.filter(
+    (validator, index, self) =>
+      index === self.findIndex((t) => t.id === validator.id)
+  )
 
-	const sortedValidators = uniqueValidators.sort((a, b) => a.id - b.id)
+  const sortedValidators = uniqueValidators.sort((a, b) => a.id - b.id)
 
-	return {
-		props: {
-			validators: sortedValidators,
-		},
-	}
+  return {
+    props: {
+      validators: sortedValidators,
+    },
+  }
 }
 
 interface IProps {
-	validators: IValidator[]
+  validators: IValidator[]
 }
 
 const ValidatorsPage = ({ validators }: IProps) => {
-	const theme = useTheme()
-	const isMd = useMediaQuery(`(min-width: ${theme.breakpoints.md})`)
+  const { asPath } = useRouter()
+  const theme = useTheme()
+  const isMd = useMediaQuery(`(min-width: ${theme.breakpoints.md})`)
 
-	return (
-		<>
-			<Head>
-				<title>Validator Alliance</title>
-				<meta
-					name="description"
-					content="We are a group of community leaders who decided to dedicate their professional lives to the Polkadot and its diverse ecosystem"
-				/>
-			</Head>
+  return (
+    <>
+      <Head
+        title="Validator Alliance"
+        description="We are a group of community leaders who decided to dedicate their professional lives to the Polkadot and its diverse ecosystem"
+        image={`https://dotvalidators.org/assets/meta-image.png`}
+        url={`https://dotvalidators.org${asPath}`}
+      >
+        <link rel="canonical" href={`https://dotvalidators.org${asPath}`} />
+        <meta property="og:type" content="website" />
+      </Head>
 
-			<ValidatorSection data={validators} enableAnimation={isMd} />
-		</>
-	)
+      <ValidatorSection data={validators} enableAnimation={isMd} />
+    </>
+  )
 }
 
 export default ValidatorsPage
 
 ValidatorsPage.getLayout = function getLayout(page: ReactElement) {
-	return <PageLayout>{page}</PageLayout>
+  return <PageLayout>{page}</PageLayout>
 }
 
 const Container = styled.div`
-	section {
-		&:nth-child(1) {
-			margin-top: 2rem;
-		}
-	}
+  section {
+    &:nth-child(1) {
+      margin-top: 2rem;
+    }
+  }
 `
